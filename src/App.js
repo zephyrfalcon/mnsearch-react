cd2b6c99f9fe19e794b908b3aa94aee5b082840c
Hans <zephyrfalcon@gmail.com>
2018-11-17 21:48:49 -0500

    display card names alphabetically

--- src/App.js
+++ src/App.js
@@ -2,6 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 import carddata from './cards.json';
 
+class QueryArea extends Component {
+  render() {
+    return '';
+  }
+}
+
+/* props:
+   - cards: list of JSON card data
+*/
+class SearchResults extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <table className="SearchResults-cards">
+        {[].concat(this.props.cards)
+         .sort((a, b) => (a.name.toLowerCase() < b.name.toLowerCase()) ? -1 : 1)
+         .map(card => 
+           <tr key={card.key}>
+             <td>{card.name || "?!?"}</td>
+           </tr>
+        )}
+      </table>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -9,6 +37,8 @@ class App extends Component {
           <p>
             Now with data for {carddata.length} cards!
           </p>
+          <QueryArea />
+          <SearchResults cards={carddata} />
       </div>
     );
   }
