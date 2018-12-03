55d4e733ad2fb00f3604a2a38344f76b25a523c6
Hans <zephyrfalcon@gmail.com>
2018-12-03 10:28:05 -0500

    version 0.8.1; added changelog

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { sortBy } from 'lodash';
 
 // carddata.sort((a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1);
 
-const VERSION = "0.8";
+const VERSION = "0.8.1";
 
 const SORTS = {
   name: list => sortBy(list, [(card) => card.name.toLowerCase()]),
